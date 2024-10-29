d7071f7aeecae40c7487d38af75fae7957eb0611
Iacopo Filiberto <iacopo.filiberto@gmail.com>
2024-10-29 09:22:52 +0100

    chore: spostate view dentro lo script di init del database

--- db/init.sql
+++ db/init.sql
@@ -38,3 +38,12 @@ create table user_game
 	purchase_date timestamp,
 	hours_played numeric(8,2)
 );
+
+CREATE VIEW game_owned_stats as SELECT g.name as game_name, COUNT(ug.game) as user_count
+                                FROM games g
+                                         JOIN user_game ug on ug.game = g.code
+                                GROUP BY g.name;
+CREATE VIEW achievement_obtained_stats as SELECT a.name as achievement_name, COUNT(ua.user) as user_count, COALESCE(ROUND(AVG(ua.unlocked_at_played_hours),2),0) as avg_unlocked_at_played_hours
+                                          FROM achievements a
+                                                   LEFT JOIN user_achievement ua on ua.achievement = a.name
+                                          GROUP BY a.name;
--- src/main/sql/access.sql
+++ src/main/sql/access.sql
@@ -53,14 +53,6 @@ GRANT delete ON users, games, achievements, user_game, user_achievement TO data_
 
 
 -- 4)
-CREATE VIEW game_owned_stats as SELECT g.name as game_name, COUNT(ug.game) as user_count
-                                FROM games g
-                                         JOIN user_game ug on ug.game = g.code
-                                GROUP BY g.name;
-CREATE VIEW achievement_obtained_stats as SELECT a.name as achievement_name, COUNT(ua.user) as user_count, COALESCE(ROUND(AVG(ua.unlocked_at_played_hours),2),0) as avg_unlocked_at_played_hours
-                                          FROM achievements a
-                                                   LEFT JOIN user_achievement ua on ua.achievement = a.name
-                                          GROUP BY a.name;
 GRANT select ON game_owned_stats TO game_owner;
 GRANT select ON achievement_obtained_stats TO game_owner;
 
